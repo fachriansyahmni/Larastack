62db41b4eda1e5e21175e701142fa5caaf0207b3
fachriansyahmni <fachriansyahmni@gmail.com>
2020-07-11 21:23:05 +0700

    tambah list pertanyaan yang pernah ditanyakan pada page profile

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Pertanyaan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -15,7 +16,8 @@ class HomeController extends Controller
     public function index()
     {
         $data = DB::table('users')->join('profile', 'users.id', '=', 'profile.user_id')->where('users.id', Auth::user()->id)->first();
-        return view('profile.index', compact('data'));
+        $pertanyaan = Pertanyaan::where('penanya_id', $data->id)->get();
+        return view('profile.index', compact('data', 'pertanyaan'));
     }
     public function edit()
     {
@@ -23,16 +25,17 @@ class HomeController extends Controller
         return view('profile.edit', compact('data'));
     }
 
-    public function update($user_id, Request $request){
-        
+    public function update($user_id, Request $request)
+    {
+
         $jawaban = DB::table('profile')
-                        ->where('user_id', $user_id)
-                        ->update([
-                            'nama_lengkap' => $request->nama_lengkap,
-                            'user_id' => $request->user_id,
-                            'gender'=>$request->gender,
-                            'tanggal_lahir'=>$request->tanggal_lahir
-                        ]);
+            ->where('user_id', $user_id)
+            ->update([
+                'nama_lengkap' => $request->nama_lengkap,
+                'user_id' => $request->user_id,
+                'gender' => $request->gender,
+                'tanggal_lahir' => $request->tanggal_lahir
+            ]);
         // dd($jawaban);
         return redirect('/home');
     }
